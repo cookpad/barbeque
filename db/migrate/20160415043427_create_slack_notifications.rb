c1ea4ff97a4c7b68e87a18e3925f379a1b4d4556
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-07-29 09:58:22 +0900

    Specify ROW_FORMAT=dynamic for slack too

--- db/migrate/20160415043427_create_slack_notifications.rb
+++ db/migrate/20160415043427_create_slack_notifications.rb
@@ -1,6 +1,6 @@
 class CreateSlackNotifications < ActiveRecord::Migration[5.0]
   def change
-    create_table :slack_notifications do |t|
+    create_table :slack_notifications, options: 'ENGINE=InnoDB ROW_FORMAT=dynamic DEFAULT CHARSET=utf8mb4' do |t|
       t.integer :job_definition_id
       t.string :channel, null: false
       t.boolean :notify_success, default: false, null: false
