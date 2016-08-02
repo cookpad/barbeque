097117afb61a471120faba0fd96ec4751d67614d
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-08-02 17:55:53 +0900

    Load kaminari on before_configuration

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,3 @@
-require 'kaminari'
-require 'kaminari/models/active_record_model_extension'
-
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  include Kaminari::ActiveRecordModelExtension
 end
