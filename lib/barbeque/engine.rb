0f7d8c182b0bb3cfc7bc22658dd22ec29ef85279
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-08-17 18:54:04 +0900

    Add gems for assets

--- lib/barbeque/engine.rb
+++ lib/barbeque/engine.rb
@@ -3,8 +3,13 @@ module Barbeque
     isolate_namespace Barbeque
 
     config.before_configuration do
+      # Listing gems which are mountable engine or have railtie.
+      require 'adminlte2-rails'
+      require 'coffee-rails'
       require 'hamlit'
+      require 'jquery-rails'
       require 'kaminari'
+      require 'sass-rails'
       require 'weak_parameters'
     end
   end
