1233527509ea0c6c2212583d9151734633cd2706
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-08-17 16:28:46 +0900

    Move assets to barbeque namespace

--- app/assets/javascripts/barbeque/application.js
+++ app/assets/javascripts/barbeque/application.js
@@ -10,4 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require app
 //= require_tree .
