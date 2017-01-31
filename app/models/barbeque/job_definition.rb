c63c2c4bb5663a692145b08e29d6487e23d78aa5
Kohei Suzuki <eagletmt@gmail.com>
2017-01-31 16:44:03 +0900

    Use Range#step to enumerate hours

--- app/models/barbeque/job_definition.rb
+++ app/models/barbeque/job_definition.rb
@@ -17,7 +17,7 @@ class Barbeque::JobDefinition < Barbeque::ApplicationRecord
   def execution_stats(from, to)
     from = from.beginning_of_hour
     to = to.beginning_of_hour
-    stats = {}
+    stats = Hash.new { |h, k| h[k] = { count: 0, avg_time: 0 } }
     job_executions.where(created_at: from .. to).group(DATE_HOUR_SQL).order(DATE_HOUR_SQL).pluck("#{DATE_HOUR_SQL}, count(1), avg(timestampdiff(second, created_at, finished_at))").each do |date_hour, count, avg_time|
       time = Time.zone.parse("#{date_hour} UTC")
       stats[time] = {
@@ -25,15 +25,9 @@ class Barbeque::JobDefinition < Barbeque::ApplicationRecord
         avg_time: avg_time,
       }
     end
-    t = from
-    while t < to
-      stats[t] ||= {
-        count: 0,
-        avg_time: 0,
-      }
-      t += 1.hour
+    (from.to_i ... to.to_i).step(1.hour).map do |t|
+      time = Time.at(t)
+      stats[time].merge(date_hour: time)
     end
-
-    stats.keys.sort.map { |time| stats[time].merge(date_hour: time) }
   end
 end
