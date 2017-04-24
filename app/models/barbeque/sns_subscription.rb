d0e0e92828b088e5f2ed054fe845e2909046c937
Shimpei Makimoto <makimoto@tsuyabu.in>
2017-04-24 11:47:17 +0900

    Set validations to SNSSubscription

--- app/models/barbeque/sns_subscription.rb
+++ app/models/barbeque/sns_subscription.rb
@@ -2,5 +2,11 @@ module Barbeque
   class SNSSubscription < ApplicationRecord
     belongs_to :job_queue
     belongs_to :job_definition
+
+    validates :topic_arn,
+      uniqueness: { scope: :job_queue, message: 'should be set with only one queue' },
+      presence: true
+    validates :job_queue, presence: true
+    validates :job_definition, presence: true
   end
 end
