4a1e16d6e2dc55c1054b32ca71ea6b3193296110
Kohei Suzuki <eagletmt@gmail.com>
2018-04-12 18:52:22 +0900

    Set request.format = :json
    Regression in #67

--- app/controllers/barbeque/api/application_controller.rb
+++ app/controllers/barbeque/api/application_controller.rb
@@ -1,6 +1,8 @@
 require 'garage'
 
 class Barbeque::Api::ApplicationController < ActionController::API
+  before_action :force_json_format
+
   include Garage::ControllerHelper
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
@@ -18,5 +20,10 @@ class Barbeque::Api::ApplicationController < ActionController::API
   # @param [String] message Error message for API client, not for end user.
   def respond_with_error(status_code, error_code, message)
     render json: { status_code: status_code, error_code: error_code, message: message }, status: status_code
+  end
+
+  # This is required to use ActionController::API with Garage
+  def force_json_format
+    request.format = :json
   end
 end
