05bbf4f3437ffee9e2ec98dc867609766cff429f
Kohei Suzuki <eagletmt@gmail.com>
2018-09-26 17:52:23 +0900

    Stop deleting job executions
    Job executions tend to have large number of records, so deleting them is
    impossible.

--- app/controllers/barbeque/job_retries_controller.rb
+++ app/controllers/barbeque/job_retries_controller.rb
@@ -2,6 +2,9 @@ class Barbeque::JobRetriesController < Barbeque::ApplicationController
   def show
     @job_retry = Barbeque::JobRetry.find(params[:id])
     @job_execution = @job_retry.job_execution
+    # Return 404 when job_definition or app is deleted
+    @job_definition = Barbeque::JobDefinition.find(@job_execution.job_definition_id)
+    @app = Barbeque::App.find(@job_definition.app_id)
 
     if params[:job_execution_message_id] != @job_execution.message_id
       redirect_to([@job_execution, @job_retry])
